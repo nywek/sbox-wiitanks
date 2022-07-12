544210408d93e73e547af1ba49223f12b1e4ee3c
Pingebam <kontakt@pingebam.de>
2022-07-12 21:43:52 +0200

    Style: Fix indentation

--- code/generator/ArenaGridConfiguration.cs
+++ code/generator/ArenaGridConfiguration.cs
@@ -2,6 +2,6 @@ namespace WiiTanks;
 
 public static class ArenaGridConfiguration
 {
-  public const int ARENA_WIDTH = 26;
+	public const int ARENA_WIDTH = 26;
 	public const int ARENA_HEIGHT = 15;
 }
