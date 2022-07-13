0c8d84d916dfe71f0dc0cbfb5efa0d0814c907b5
Pingebam <kontakt@pingebam.de>
2022-07-13 23:14:49 +0200

    Add Pathway tile and refactor TileResolver

--- code/generator/TileResolver.cs
+++ code/generator/TileResolver.cs
@@ -2,34 +2,91 @@ namespace WiiTanks;
 
 public class TileResolver
 {
-	public const int OUTER_SPACE = -1;
+	public const int INVALID = -1;
 	public const int EMPTY = 0;
 	public const int STATIC_WALL = 1;
 	public const int DYNAMIC_WALL = 2;
-	public const int ROOM = 3;
+	public const int ROOM = 4;
+	public const int PATHWAY = 8;
+
+	public const int OPEN = EMPTY | ROOM | PATHWAY;
+	public const int BLOCKING = STATIC_WALL | DYNAMIC_WALL;
 
 	public static bool IsValid(int tile)
 	{
-		return tile != OUTER_SPACE;
+		return tile != INVALID;
 	}
 
 	public static bool IsEmpty(int tile)
 	{
-		return tile == EMPTY;
+		return IsValid(tile) && tile == EMPTY;
 	}
 
-	public static bool IsWall(int tile)
+	public static bool IsStaticWall(int tile)
 	{
-		return tile == STATIC_WALL || tile == DYNAMIC_WALL;
+		return IsValid(tile) && (tile & STATIC_WALL) != 0;
+	}
+
+	public static bool IsDynamicWall(int tile)
+	{
+		return IsValid(tile) && (tile & DYNAMIC_WALL) != 0;
 	}
 
 	public static bool IsRoom(int tile)
 	{
-		return tile == ROOM;
+		return IsValid(tile) && (tile & ROOM) != 0;
+	}
+
+	public static bool IsPathway(int tile)
+	{
+		return IsValid(tile) && (tile & PATHWAY) != 0;
+	}
+
+	public static bool IsWall(int tile)
+	{
+		return IsStaticWall(tile) || IsDynamicWall(tile);
+	}
+
+	public static bool IsOpen(int tile)
+	{
+		return IsValid(tile) && (tile & OPEN) != 0;
 	}
 
 	public static bool IsBlocking(int tile)
 	{
-		return !IsValid(tile) || (!IsEmpty(tile) && !IsRoom(tile));
+		return !IsValid(tile) || (tile & BLOCKING) != 0;
+	}
+
+	public static int ApplyOpenBlockingUnsafe(int source, int tile)
+	{
+		if (!IsValid(source) || !IsValid(tile))
+		{
+			return INVALID;
+		}
+		return source | tile;
+	}
+
+	public static int Apply(int source, int tile)
+	{
+		int appliedTile = ApplyOpenBlockingUnsafe(source, tile);
+		if (!IsValid(appliedTile))
+		{
+			return INVALID;
+		}
+		// Result would be in open and blocking state at the same time, but applying tile is not
+		if (!(IsOpen(tile) && IsBlocking(tile)) && IsOpen(appliedTile) && IsBlocking(appliedTile))
+		{
+			// Remove the blocking tiles, when applying tile is in open state
+			if (IsOpen(tile))
+			{
+				appliedTile &= ~BLOCKING;
+			}
+			// Remove the open tiles, when applying tile is in blocking state
+			if (IsBlocking(tile))
+			{
+				appliedTile &= ~OPEN;
+			}
+		}
+		return appliedTile;
 	}
 }
