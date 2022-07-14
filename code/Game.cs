f470a00c9bd482973b63427ac08e116e3e1b8b0a
nywek <nywekk@gmail.com>
2022-07-14 20:04:57 +0200

    Tick active round, pass joined clients to lobby

--- code/Game.cs
+++ code/Game.cs
@@ -16,19 +16,47 @@ public partial class TankGame : Sandbox.Game
 	{
 		base.PostLevelLoaded();
 
-		var arena = Entity.All.OfType<Arena>().First();
-		Round = new Round( arena );
-
+		Host.AssertServer();
+		
 		Lobby = new Lobby();
+	}
+
+	[Event.Tick.Server]
+	public void OnTick()
+	{
+		Round?.Tick();
 	}
 
 	public override void ClientJoined( Client client )
 	{
 		base.ClientJoined( client );
 
-		var tank = new Tank( client, Team.BLUE );
-		var spawn = Round.Arena.FindSpawnPoints().First();
+		if ( Round is null )
+		{
+			// As long as no round exists, just randomly spawn tanks
+			
+			var tank = new Tank( client, Team.BLUE );
+			var randomArena = Entity.All.OfType<Arena>().First();
+			var spawn = randomArena.FindSpawnPoints().First();
 
-		tank.SpawnAtArena( Round.Arena, spawn.Position );
+			tank.SpawnAtArena( randomArena, spawn.Position );
+
+			tank.LifeState = LifeState.Alive;
+		}
+		
+		Lobby.Join( client );
+
+		if ( Lobby.IsFull )
+		{
+			// Begin round, kill all existing tanks
+			foreach (var tank in Entity.All.OfType<Tank>().ToList())
+			{
+				tank.Delete();
+			}
+			
+			Round = Lobby.CreateRound();
+
+			Round.Begin();
+		}
 	}
 }
